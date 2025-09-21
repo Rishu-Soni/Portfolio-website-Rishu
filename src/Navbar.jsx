eafabbd3fdd303b7209eb3809cd9fa50f430df25
Rishu Soni <rishusoni6393@gmail.com>
2025-09-21 22:02:52 +0530

    further developing the experience page

---
+++ src/Navbar.jsx
@@ -0,0 +1,38 @@
+import "./assets/CSS/navbar.css";
+import "./assets/Navbar_assets/rishu_logo.png";
+import "./assets/Navbar_assets/rishu_logo.png";
+function Navbar() {
+  return (
+      <nav className="navbar">
+        <div className="logoContainer">
+          <img
+            src="https://imgv3.fotor.com/images/videoImage/wonderland-girl-generated-by-Fotor-ai-art-generator.jpg"
+            alt="Portfolio-Logo"
+            className="logoImage"
+          />
+          <h1>Rishu's Portfolio</h1>
+        </div>
+
+        <ul className="navLinks">
+          <li>
+            {" "}
+            <a href="Portfolio_Rishu\index.html"> Home </a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a href=""> About </a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a href=""> Projects </a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a href=""> Contact </a>{" "}
+          </li>
+        </ul>
+      </nav>
+  );
+}
+
+export default Navbar;
